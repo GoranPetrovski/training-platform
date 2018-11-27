861e5547da443285a977a73c52f8fb79151e2d38
goran.petrovski <goki.petrovski@gmail.com>
2018-11-27 17:18:05 +0100

    adding .gitignore file'

--- src/main/java/com/training/project/app/HelloApplication.java
+++ src/main/java/com/training/project/app/HelloApplication.java
@@ -3,10 +3,12 @@ package com.training.project.app;
 import com.training.project.rest.HelloRestService;
 import com.training.project.rest.TrainingController;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
+@ApplicationPath("api")
 public class HelloApplication extends Application {
     private Set<Object> singletons = new HashSet<Object>();
     public HelloApplication() {
